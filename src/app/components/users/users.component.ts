3dabe10a1169960eb923cf912ea4c35fe3c76361
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2020-04-26 20:00:34 -0400

    17. Ng For Loops

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-
+import {User} from '../../models/User';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users:User[];
   constructor() { }
 
   ngOnInit(): void {
+    this.users=[
+      {
+        firstName: 'Rick',
+        lastName: 'Sanchez',
+        age: 20,
+        address:{
+          street: '50 Main st.',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Jerry',
+        lastName: 'Sanchez',
+        age: 21,
+        address:{
+          street: '51 Main st.',
+          city: 'Boston',
+          state: 'FL'
+        }
+      },
+      {
+        firstName: 'Morty',
+        lastName: 'Sanchez',
+        age: 22,
+        address:{
+          street: '52 Main st.',
+          city: 'Boston',
+          state: 'OH'
+        }
+      }
+    ];
+
+      this.adduser({
+        firstName: 'Beth',
+        lastName: 'Sanchez',
+        age: 23,
+        address:{
+          street: '53 Main st.',
+          city: 'Columbus',
+          state: 'OH'
+        }
+      })
+    
+  }
+  adduser(user:User){
+    this.users.push(user);
   }
 
 }
